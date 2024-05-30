df677d21d1c68b170b33d1c1376c4b65c371e9dd
Love <mzeelovegeneral@gmail.com>
2024-05-30 23:18:00 +0100

    doc(readme) and functiontion to print pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Function to print a Pascal Triangle """
+
+def pascal_triangle(n):
+    """
+    Create a function that returns a list of lists
+    of integers representing Pascal’s triangle of n levels.
+    """
+    triangle = []
+    if n > 0:
+        for row_number in range(1, n + 1):
+            row = []
+            value = 1
+            for position in range(1, row_number + 1):
+                row.append(value)
+                value = value * (row_number - position) // position
+            triangle.append(row)
+    return triangle
+
+
+print(pascal_triangle(5))
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+# Welcome to the 0x00-pascal_triangle directory
