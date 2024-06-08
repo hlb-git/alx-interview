1364d79c757a597f7b8782bce3fddc5c8c3108b1
Love <mzeelovegeneral@gmail.com>
2024-06-08 23:11:11 +0100

    lockboxes puzzle and readme documentation

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Solves the lock boxes task """
+
+
+def look_next_opened_box(opened_boxes):
+    """checks next box"""
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
+
+
+def canUnlockAll(boxes):
+    """checks if it can be unlocked"""
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
+
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {
+                'status': 'opened',
+                'keys': boxes[0],
+            }
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    aux[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
+
+    return len(aux) == len(boxes)
+
+
+def main():
+    """Main Entry point"""
+    canUnlockAll([[]])
+
+
+if __name__ == '__main__':
+    main()
