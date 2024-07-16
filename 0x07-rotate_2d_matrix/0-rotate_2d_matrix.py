39d834bcff3ca5a156e31fe8088a2f03979c7de8
hlb-git <mzeelovegeneral@gmail.com>
2024-07-16 21:39:41 +0100

    rotate 2d matrix clockwise 90 degrees

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix """
+       
+
+def rotate_2d_matrix(m):
+    """main function: rotates a 2d array 90 deg"""
+    width = len(m[0])
+    height = len(m)
+    total_items = width * height
+    map_ = build_map(m, total_items, width)
+    # print(map_)
+    for index in map_:
+        prev_index = index
+        value = map_[prev_index]["value"]
+        new_index = map_[prev_index]["new_index"]
+        row, index = compute_row_and_index(new_index, width)
+        m[row][index] = value
+
+
+def build_map(m, length, width):
+    """returns a map of prev_index and their
+    next positions"""
+    dct = {
+        i: {
+            "new_index": get_new_index(i, width),
+            "value": get_value(m, i, width)
+        }
+        for i in range(length)
+    }
+    return dct
+
+
+def get_new_index(i, width):
+    """computes new position"""
+    current_index = i % width
+    current_row = int(i / width)
+    next_index = width - 1 - current_row
+    next_row = current_index
+    next_position = next_row * width + next_index
+    return next_position
+
+
+def get_value(m, i, width):
+    """gets the value of a 2d array by denormalizing
+    its flattened index"""
+    current_index = i % width
+    current_row = int(i / width)
+    return m[current_row][current_index]
+
+
+def compute_row_and_index(flat_index, width):
+    """returns denormalized row and index from a
+    flattened 2d array index"""
+    row = int(flat_index / width)
+    index = flat_index % width
+    return row, index
+
+
+def printx(lst):
+    """custom print function to print 2d array"""
+    print("[")
+    for itm in lst:
+        print("  ", str(itm) + ",")
+    print("]")
+
+
+def build_matrix(n):
+    """ builds n * n 2d matrix """
+    parent = []
+    y = 1
+    for i in range(n):
+        child = []
+        for x in range(y, n * n + 1):
+            child.append(x)
+            if x % n == 0 and x != 1:
+                y = x + 1
+                break
+        parent.append(child)
+    return parent
+
+
+if __name__ == "__main__":
+    matrix = build_matrix(3)
+
+    rotate_2d_matrix(matrix)
+    printx(matrix)
+    print()
+    matrix = build_matrix(5)
+
+    rotate_2d_matrix(matrix)
+    printx(matrix)
